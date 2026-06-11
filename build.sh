3e07a0b9c1176a2845b859ea6db6bd459761482e
Wolfgang Kulhanek <wkulhane@redhat.com>
2026-06-11 17:23:36 +0200

    Updated build.sh to clean up images before building new

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 IMAGE="${1:-quay.io/wkulhanek/bmw-loxone-bridge:latest}"
 
 podman manifest rm "$IMAGE" 2>/dev/null || true
+podman rmi "$IMAGE" 2>/dev/null || true
+podman manifest create "$IMAGE"
 
 podman build --platform linux/amd64,linux/arm64 --manifest "$IMAGE" .
 
